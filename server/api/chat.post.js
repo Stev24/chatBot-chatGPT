8891713aecfc3373c3c33e8135a3bc5d33cf23f7
Steven Ribeiro <steven24ribeiro@gmail.com>
2023-11-29 14:05:16 +0100

    bot version 1 created

--- server/api/chat.post.js
+++ server/api/chat.post.js
@@ -1,31 +1,54 @@
-export default defineEventHandler(async (event) => {
-	const config = useRuntimeConfig();
-	let messages = [];
-	const previosMessages = await readBody(event);
-	messages = messages.concat(previosMessages);
-	let prompt =
-		messages.map((message) => `${message.role}: ${message.message}`).join('\n') + `\nAI:`;
-	const req = await fetch('https://api.openai.com/v1/completions', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${config.OPENAI_API_KEY}`
-		},
-		body: JSON.stringify({
-			model: 'text-davinci-003',
-			prompt: prompt,
-			temperature: 0.9,
-			max_tokens: 512,
-			top_p: 1.0,
-			frequency_penalty: 0,
-			presence_penalty: 0.6,
-			stop: [' User:', ' AI:']
-		})
-	});
+import OpenAI from "openai";
 
-	const res = await req.json();
-	const result = res.choices[0];
-	return {
-		message: result.text
-	};
+const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"], // defaults to process.env["OPENAI_API_KEY"]
+});
+
+let thread = undefined;
+
+export default defineEventHandler(async (event) => {
+  const previosMessages = await readBody(event);
+
+  // Create a thread
+  if (!thread) {
+	console.log('initiate thread', thread);
+	thread = await openai.beta.threads.create();
+  }
+  
+  // Pass in the user question into the existing thread
+  await openai.beta.threads.messages.create(thread.id, {
+    role: "user",
+    content: previosMessages.pop()?.message,
+  });
+
+  // Use runs to wait for the assistant response and then retrieve it
+  const run = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: process.env["OPENAI_ASSISTANT_ID"],
+  });
+   
+  console.log(thread.id, run.id);
+  let runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+
+  // Polling mechanism to see if runStatus is completed
+  // This should be made more robust.
+  while (runStatus.status !== "completed") {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    runStatus = await openai.beta.threads.runs.retrieve(thread.id, run.id);
+  }
+
+  // Get the last assistant message from the messages array
+  const messages = await openai.beta.threads.messages.list(thread.id);
+
+  // Find the last message for the current run
+  const lastMessageForRun = messages.data
+    .filter(
+      (message) => message.run_id === run.id && message.role === "assistant"
+    )
+    .pop();
+
+	console.log(lastMessageForRun.content[0].text.value);
+
+  return {
+    message: lastMessageForRun.content[0].text.value,
+  };
 });
